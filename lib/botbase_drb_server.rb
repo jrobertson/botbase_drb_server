ac568b39f4a80f3ac6030e83ab4d25bcc573ba08
Your Name <you@example.com>
2017-10-30 00:13:56 +0000

    feature: The log object for the botbase object is now passed in optionally through initialize().

--- lib/botbase_drb_server.rb
+++ lib/botbase_drb_server.rb
@@ -10,16 +10,20 @@ require 'botbase'
 class BotBaseDRbServer
 
   def initialize(host: 'localhost', port: '60600', config: '', 
-                 notifier: nil, debug: false)
+                 notifier: nil, log: nil)
 
-    @host, @port = host, port
-    @bot = BotBase.new(config, notifier: notifier, debug: debug)
-
+    log.info 'BotBaseDRbServer/initialize: active' if log
+    
+    @host, @port, @log = host, port, log
+    @bot = BotBase.new(config, notifier: notifier, log: log)
+    
+    log.info 'BotBaseDRbServer/initialize: completed' if log
 
   end
 
   def start()
 
+    @log.info 'BotBaseDRbServer/start: active' if @log
     DRb.start_service "druby://#{@host}:#{@port}", @bot
     DRb.thread.join
 
